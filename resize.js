1675a6c2e4f292a384668dae9d1446bfe2a27a63
trangtt-monster <trangtt@monster.io>
2023-09-11 17:21:15 +0700

    done

--- resize.js
+++ resize.js
@@ -1,6 +1,7 @@
 const fileInput = document.getElementById('select-image');
 const images = document.getElementById('images');
 const totalImages = document.getElementById('total-images');
+const downloadBtn = document.getElementById('download');
 
 // Listen to the change event on the <input> element
 fileInput.addEventListener('change', (event) => {
@@ -27,7 +28,7 @@ fileInput.addEventListener('change', (event) => {
                 // Create new <img> element and add it to the DOM
                 images.innerHTML += `
                 <div class="image_box">
-                    <img src='${reader.result}'>
+                    <img class='resized_image' src='${reader.result}'>
                     <span class='image_name'>${imageFile.name}</span>
                 </div>
             `;
@@ -38,3 +39,93 @@ fileInput.addEventListener('change', (event) => {
         images.innerHTML = '';
     }
 });
+
+downloadBtn.addEventListener('click', async () => {
+    const imgs = document.querySelectorAll('.resized_image');
+
+    if (imgs.length < 1) return;
+    
+    const zip = new JSZip();
+    imgs.forEach((el) => {
+        zip.file(el.nextElementSibling.textContent, dataURLToBlob(resizeImage(el)));
+    });
+
+    const zipFile = await zip.generateAsync({ type: 'blob'});
+    downloadZip(zipFile);
+
+});
+
+function resizeImage(imgToResize) {
+    const canvas = document.createElement("canvas");
+    const context = canvas.getContext("2d");
+  
+    let originalWidth = imgToResize.width;
+    let originalHeight = imgToResize.height;
+
+    console.log("origin width " + originalWidth);
+    console.log("origin height " + originalHeight);
+
+    while (originalWidth % 4 != 0)
+    {
+        originalWidth = originalWidth - 1;
+    }
+
+    while (originalHeight % 4 != 0)
+    {
+        originalHeight = originalHeight - 1;
+    }
+  
+    canvas.width = originalWidth;
+    canvas.height = originalHeight;
+  
+    context.drawImage(
+      imgToResize,
+      0,
+      0,
+      originalWidth,
+      originalHeight
+    );
+  
+    return canvas.toDataURL();
+}
+
+
+function downloadZip(file)
+{
+	const a = document.createElement('a');
+	a.download = 'test.zip';
+	const url = URL.createObjectURL(file);
+	a.href = url;
+	
+	a.style.display = 'none';
+
+	document.body.appendChild(a);
+	a.click();
+	a.remove();
+	URL.revokeObjectURL(url);
+
+}
+
+function dataURLToBlob (dataURL) {
+	var index = dataURL.indexOf(',');
+	var meta = dataURL.substring(0, index);
+	var data = dataURL.substring(index + 1);
+	var contentType = meta.substring(meta.indexOf(':') + 1);
+
+	if (/;base64$/.test(contentType)) {
+		contentType = contentType.substring(0, contentType.length - 7);
+		var strdata = atob(data);
+
+		data = new Uint8Array(strdata.length);
+
+		for (var i = 0; i < strdata.length; ++ i) {
+			data[i] = strdata.charCodeAt(i);
+		}
+	}
+	else {
+		data = decodeURIComponent(data);
+	}
+
+	return new Blob([data], {type: contentType});
+}
+
